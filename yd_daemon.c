a07605738d8e69b620eb7b039b7a5d2579f5dbb5
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-12 08:58:46 +1000

    Updated yd_daemon.c

--- yd_daemon.c
+++ yd_daemon.c
@@ -1,5 +1,5 @@
 /**
- * File              : daemon.c
+ * File              : yd_daemon.c
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 20.07.2022
  * Last Modified Date: 12.09.2022
@@ -13,7 +13,7 @@
 #include <unistd.h>  //for sleep
 
 #include "kdata.h"
-#include "sqlite2yandexdisk.h"
+#include "yd.h"
 #include "SQLiteConnect/SQLiteConnect.h"
 #include "cYandexDisk/cYandexDisk.h"
 
