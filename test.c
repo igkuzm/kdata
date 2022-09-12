bce77bc587a17b7f89ce7c8eb9bcfec13aa112a9
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-12 15:35:35 +1000

    Updated test.c

--- test.c
+++ test.c
@@ -9,31 +9,47 @@
 #include "kdata.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-//columns
-const kdata_column ID = {
-	.type = DTYPE_INT,
-	.key = "ID"
-};
 
-const kdata_column NAME = {
-	.type = DTYPE_TEXT,
-	.key = "NAME"
-};
-
-const kdata_column DATE = {
-	.type = DTYPE_INT,
-	.key = "DATE"
-};
+#define DATABASE "my_data.db"
 
 //table
-kdata_s * my_table() {
-	kdata_s * table = malloc(sizeof(kdata_s));
+void my_table_init(kdata_table * table) {
+	kdata_table_init(table, "my_table", 
+		DTYPE_INT, "ID",
+		DTYPE_TEXT, "NAME",
+		DTYPE_INT, "DATE",
+		NULL
+	);
+}
+
+int add_callback(void *user_data, char *uuid, kerr err){
+	char **_uuid = user_data;
+	if (err)
+		printf("%s\n", kdata_parse_kerr(err));
+	else
+		strcpy(*_uuid, uuid);
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
 	printf("Starting KDATA test...\n");
+	
+	//init database structure
+	kdata_table my_table;
+	my_table_init(&my_table);
+	kdata_s * s = kdata_structure_init();
+	kdata_structure_add(s, &my_table);
+
+	//init database
+	kdata_init(DATABASE, s, DSERVICE_LOCAL, NULL, NULL, NULL);
+
+	//add new item
+	char uuid[37];
+	kdata_add(DATABASE, my_table.tablename, &uuid, add_callback);
+	printf("UUID: %s\n", uuid);
 	
 	printf("Done. press any key to exit\n");
 	getchar();
