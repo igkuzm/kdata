16d56dc8ed6d1f4fbf7909dcf485eb09d50d2dfb
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-12 14:17:23 +1000

    Updated test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+/**
+ * File              : test.c
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 12.09.2022
+ * Last Modified Date: 12.09.2022
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+
+#include "kdata.h"
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
+
