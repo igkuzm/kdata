a51a3d87c822b3fe2258842750deee12922480ef
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-12 15:36:28 +1000

    Updated test.c

--- test.c
+++ test.c
@@ -40,8 +40,8 @@ int main(int argc, char *argv[])
 	//init database structure
 	kdata_table my_table;
 	my_table_init(&my_table);
-	kdata_s * s = kdata_structure_init();
-	kdata_structure_add(s, &my_table);
+	//kdata_s * s = kdata_structure_init();
+	//kdata_structure_add(s, &my_table);
 
 	//init database
 	//kdata_init(DATABASE, s, DSERVICE_LOCAL, NULL, NULL, NULL);
